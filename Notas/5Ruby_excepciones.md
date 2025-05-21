78955a9ebbf9784d3656162c549f948a7abe5cff
yesid <davidjuradoyt@gmail.com>
2025-05-21 11:25:03 -0500

    excepciones

--- Notas/5Ruby_excepciones.md
+++ Notas/5Ruby_excepciones.md
@@ -27,3 +27,18 @@ end
 
 puts dividir(10,0)
 ```
+
+## Creación de excepciones personalizadas
+```ruby
+class EdadInvalidaError < StandardError
+  def registrar_usuario(nombre,edad)
+    raise EdadInvalidaError, "La edad debe ser un número mayor que cero" if edad <= 0
+    puts "Usario #{nombre} registrado con la edad #{edad}"
+  rescue EdadInvalidaError => e
+    puts "Error al registrar el usuario: #{e.message}"
+  end
+end
+
+edad = EdadInvalidaError.new
+edad.registrar_usuario("Juan", 0)
+```
--- Pruebas.rb
+++ Pruebas.rb
@@ -1,14 +1,11 @@
-def dividir(a,b)
-    begin 
-        resultado = a/b
-        puts resultado
-    rescue ZeroDivisionError => e
-        puts "No se puede dividir por cero: #{e.message}"
-    rescue => e
-        puts "No se puede dividir: #{e.message}"
-    ensure
-        puts "Fin del programa"
-    end
+class EdadInvalidaError < StandardError
+  def registrar_usuario(nombre,edad)
+    raise EdadInvalidaError, "La edad debe ser un número mayor que cero" if edad <= 0
+    puts "Usario #{nombre} registrado con la edad #{edad}"
+  rescue EdadInvalidaError => e
+    puts "Error al registrar el usuario: #{e.message}"
+  end
 end
 
-puts dividir(10,0)
+edad = EdadInvalidaError.new
+edad.registrar_usuario("Juan", 0)
